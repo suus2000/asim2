7a708dff2bf82b02b697d1c6fe4dd736457414f7
Tarik Bousair <T.Bousair@student.tudelft.nl>
2024-02-29 17:45:21 +0100

    Added CSV file with cleaned data for N1 that is ready for the model

--- model/model.py
+++ model/model.py
@@ -74,7 +74,7 @@ class BangladeshModel(Model):
         Warning: the labels are the same as the csv column labels
         """
 
-        df = pd.read_csv('../data/demo-1.csv')
+        df = pd.read_csv('../data/N1_data.csv')
 
         # a list of names of roads to be generated
         roads = ['N1']
